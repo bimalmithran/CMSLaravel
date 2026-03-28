d33c649600e8e5dff6d390b46fc868d0fc28329d
Bimal Mithran P B <bimalmithranpb@gmail.com>
2026-03-28 11:54:30 +0530

    Storehighlight seeder update

--- database/seeders/StoreHighlightSeeder.php
+++ database/seeders/StoreHighlightSeeder.php
@@ -12,32 +12,15 @@ class StoreHighlightSeeder extends Seeder
      */
     public function run(): void
     {
-        StoreHighlight::create([
-            'icon' => 'assets/images/shipping-icon/1.png',
-            'title' => 'Free Uk Standard Delivery',
-            'description' => 'Designated day delivery',
-            'sort_order' => 1,
-        ]);
+        $highlights = [
+            ['icon' => 'assets/images/shipping-icon/1.png', 'title' => 'Free Uk Standard Delivery',   'description' => 'Designated day delivery',        'sort_order' => 1],
+            ['icon' => 'assets/images/shipping-icon/2.png', 'title' => 'Freshyly Prepared Ingredients','description' => 'Made for your delivery date',      'sort_order' => 2],
+            ['icon' => 'assets/images/shipping-icon/3.png', 'title' => '98% Of Anta Clients',          'description' => 'Reach their personal goals set',   'sort_order' => 3],
+            ['icon' => 'assets/images/shipping-icon/4.png', 'title' => 'Winner Of 15 Awards',          'description' => 'Healthy food and drink 2022',      'sort_order' => 4],
+        ];
 
-        StoreHighlight::create([
-            'icon' => 'assets/images/shipping-icon/2.png',
-            'title' => 'Freshyly Prepared Ingredients',
-            'description' => 'Made for your delivery date',
-            'sort_order' => 2,
-        ]);
-
-        StoreHighlight::create([
-            'icon' => 'assets/images/shipping-icon/3.png',
-            'title' => '98% Of Anta Clients',
-            'description' => 'Reach their personal goals set',
-            'sort_order' => 3,
-        ]);
-
-        StoreHighlight::create([
-            'icon' => 'assets/images/shipping-icon/4.png',
-            'title' => 'Winner Of 15 Awards',
-            'description' => 'Healthy food and drink 2022',
-            'sort_order' => 4,
-        ]);
+        foreach ($highlights as $highlight) {
+            StoreHighlight::firstOrCreate(['sort_order' => $highlight['sort_order']], $highlight);
+        }
     }
 }
